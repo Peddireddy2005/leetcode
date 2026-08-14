18bdcc6396d6b9d192933b9688b260ac74b1bf56
Peddireddy2005 <peddireddyeswarreddy9999@gmail.com>
2026-08-15 01:05:01 +0530

    Time: 1 ms (82%), Space: 43.1 MB (18.13%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,26 @@
+class MyStack {
+    private Queue<Integer> q;
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q.add(x);
+        int n = q.size();
+        for (int i = 0; i < n - 1; i++) {
+            q.add(q.remove());
+        }
+    }
+
+    public int pop() {
+        return q.remove();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
